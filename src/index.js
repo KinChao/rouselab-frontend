6febfbd7553ff927cbdafc788822011e06aae507
StephenChao <mqchay001@gmail.com>
2022-10-04 07:22:56 +0100

    modified:   build/asset-manifest.json 	modified:   build/index.html 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import Router from './router'
 
 
@@ -11,7 +11,7 @@ import Router from './router'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <BrowserRouter>  
+  <HashRouter>  
     <Router />
-  </BrowserRouter>
+  </HashRouter>
 )
